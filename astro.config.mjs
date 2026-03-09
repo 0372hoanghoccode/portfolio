5a383c2c0dbe47541697e4d249525faf9b022b0d
Vũ Huy Hoàng <0372hoanghoccode@github.com>
2026-03-09 16:52:26 +0700

    fix: set base path to /portfolio for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://0372hoanghoccode.github.io',
+  base: '/portfolio',
   integrations: [react()],
 });
